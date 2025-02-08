4613d55dc5bd09935895c963b20f91976375ffae
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-02-08 22:28:03 +0530

    Addded UUID in BaseModel

--- src/main/java/com/example/productservicefeb25/models/BaseModel.java
+++ src/main/java/com/example/productservicefeb25/models/BaseModel.java
@@ -1,10 +1,15 @@
 package com.example.productservicefeb25.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 }
